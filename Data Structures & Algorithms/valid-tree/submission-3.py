f4aee666aeac4746e607ea09799400a8c3853bd9
Krish Patel <krish2007patel@gmail.com>
2026-08-18 17:58:05 +0100

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > (n-1):
+            return False
+
+        # adjacency list -> for each node, has all neighbours
+        adj = [[] for _ in range(n)]  
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        # perform our dfs search
+        visit = set()
+        def dfs(node, par):
+            if node in visit:
+                return False
+            visit.add(node)
+            for nei in adj[node]: # for each of the possible connections
+                if nei == par:
+                    continue
+                if not dfs(nei, node):
+                    return False
+            return True
+        return dfs(0, -1) and len(visit) == n
+
